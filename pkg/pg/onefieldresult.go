c1b9b0586927879833e4228502b3bb2aa7f1072c
Nils Mannem <nilsmannem36@gmail.com>
2025-04-23 19:31:40 +0200

    fixed some linting issues

--- pkg/pg/onefieldresult.go
+++ pkg/pg/onefieldresult.go
@@ -1,6 +1,7 @@
 package pg
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
 	"strings"
@@ -12,6 +13,7 @@ import (
 // ResultValueToString is a function that can return given data such as an
 // unsigned 8 bit integer and return it as a string containing a number
 func ResultValueToString(value any) string {
+	const baseTenVerb = "%d"
 	switch v := value.(type) {
 	case string:
 		return v
@@ -24,27 +26,27 @@ func ResultValueToString(value any) string {
 	case time.Time:
 		return v.String()
 	case int:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case int8:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case int16:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case int32:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case int64:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case uint:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case uint8:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case uint16:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case uint32:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case uint64:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case []byte:
-		return fmt.Sprintf("%d", v)
+		return fmt.Sprintf(baseTenVerb, v)
 	case pgtype.Float4Array:
 		var l []string
 		for _, e := range v.Elements {
@@ -67,7 +69,7 @@ type Results []Result
 func newResultFromByteArrayArray(cols []string, values []any) (ofr Result, err error) {
 	ofr = Result{}
 	if len(cols) != len(values) {
-		return ofr, fmt.Errorf("number of cols different then number of values")
+		return ofr, errors.New("number of cols different then number of values")
 	}
 	for i, col := range cols {
 		ofr[col] = ResultValueToString(values[i])
@@ -85,7 +87,8 @@ func (ofr Result) String() (s string) {
 	return fmt.Sprintf("{ %s }", strings.Join(results, ", "))
 }
 
-// Columns is a function that creates a column of all the results
+// Columns is a function that converts a Result into a list of strings.
+// For every column the string value is added into the list.
 func (ofr Result) Columns() (result []string) {
 	for key := range ofr {
 		result = append(result, key)
@@ -93,9 +96,10 @@ func (ofr Result) Columns() (result []string) {
 	return result
 }
 
-// FormattedString is a function that formats strings by replacing ' with \\
+// FormattedString is a function that returns a SQL version of the string.
+// The string is inserted in single quotes and single quotes within the string are replaced with two single quotes
 func FormattedString(s string) string {
-	return fmt.Sprintf("'%s'", strings.Replace(s, "'", "\\'", -1))
+	return fmt.Sprintf("'%s'", strings.ReplaceAll(s, "'", "\\'"))
 }
 
 // Compare is a function that compares colums in your result
@@ -111,7 +115,8 @@ func (ofr Result) Compare(other Result) (err error) {
 		}
 		if matched, err := regexp.MatchString(otherValue, value); err != nil {
 			if value != otherValue {
-				return fmt.Errorf("comparedrow is not an re, and column %s differs between row (%s), and comparedrow (%s)",
+				return fmt.Errorf(
+					"comparedrow is not an re, and column %s differs between row (%s), and comparedrow (%s)",
 					FormattedString(key),
 					FormattedString(value),
 					FormattedString(otherValue))
@@ -128,7 +133,8 @@ func (ofr Result) Compare(other Result) (err error) {
 
 func (results Results) String() (s string) {
 	var arr []string
-	if len(results) == 0 {
+	const emptyResults = 0
+	if len(results) == emptyResults {
 		return "[ ]"
 	}
 	for _, result := range results {
