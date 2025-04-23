41faa84e34a55a62469f2ac17207472243976228
Nils Mannem <nilsmannem36@gmail.com>
2025-04-23 19:31:40 +0200

    fixed some linting issues

--- pkg/pg/main.go
+++ pkg/pg/main.go
@@ -17,17 +17,17 @@ func Initialize(logger *zap.SugaredLogger) {
 // Dsn is is a keyvalue map that is used to define the connect string to a database
 type Dsn map[string]string
 
-//// identifier returns the object name ready to be used in a sql query as an object name (e.a. select * from %s)
-//func identifier(objectName string) (escaped string) {
+// // identifier returns the object name ready to be used in a sql query as an object name (e.a. select * from %s)
+// func identifier(objectName string) (escaped string) {
 //	return fmt.Sprintf("\"%s\"", strings.Replace(objectName, "\"", "\"\"", -1))
-//}
-//
-//// quotedSqlValue uses proper quoting for values in SQL queries
-//func quotedSqlValue(objectName string) (escaped string) {
-//	return fmt.Sprintf("'%s'", strings.Replace(objectName, "'", "''", -1))
-//}
+// }
+
+// // quotedSqlValue uses proper quoting for values in SQL queries
+// func quotedSqlValue(objectName string) (escaped string) {
+// 	return fmt.Sprintf("'%s'", strings.Replace(objectName, "'", "''", -1))
+// }
 
 // connectStringValue uses proper quoting for connect string values
 func connectStringValue(objectName string) (escaped string) {
-	return fmt.Sprintf("'%s'", strings.Replace(objectName, "'", "\\'", -1))
+	return fmt.Sprintf("'%s'", strings.ReplaceAll(objectName, "'", "\\'"))
 }
